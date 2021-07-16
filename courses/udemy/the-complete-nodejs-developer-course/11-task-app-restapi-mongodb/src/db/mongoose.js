c5affef3fcf924908bf917f093cbc95c2e108c18
Kerem Türkyilmaz <kerem.turkyilmaz@outlook.it>
2021-07-16 15:53:08 +0200

    courses: udemy -> add mongoose defaults options

--- courses/udemy/the-complete-nodejs-developer-course/11-task-app-restapi-mongodb/src/db/mongoose.js
+++ courses/udemy/the-complete-nodejs-developer-course/11-task-app-restapi-mongodb/src/db/mongoose.js
@@ -1,8 +1,20 @@
 import Mongoose from 'mongoose';
 
+const DB_PORT = 27017;
+const defaultOptions = {
+	url: `mongodb://127.0.0.1:${DB_PORT}`,
+	options: {
+		dbName: 'db-rest-api',
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	},
+};
+
 export default class MongooseDB {
 	constructor(options = {}) {
-		this.options = options;
+		this.options = { ...defaultOptions, ...options };
 		this.url = this.options.url;
 		this.connectionOptions = this.options.connectionOptions;
 	}
