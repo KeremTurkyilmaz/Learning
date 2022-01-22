3b01e5428ca279903cb437c9e26b8f1a7cb13ae9
Kerem Türkyilmaz <kerem.turkyilmaz@outlook.it>
2022-01-22 17:34:57 +0100

    step 0 - move store into a module

--- courses/frontend-masters/frontend-masters-vuex/vuex-sfc/src/store/index.js
+++ courses/frontend-masters/frontend-masters-vuex/vuex-sfc/src/store/index.js
@@ -1,11 +1,51 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+// fake API call //
+let inventory = {
+  chips: {
+    stock: 40,
+  },
+};
+var pingInventory = function(item) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve(inventory[item]);
+    }, 3000);
+  });
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    supply: 40,
+    isRestocking: false,
+    isDispensing: false,
+  },
+  actions: {
+    fetchFromInventory(context) {
+      context.commit('isRestocking', true);
+      pingInventory('chips')
+        .then((inventory) => {
+          context.commit('stockItems', inventory.stock);
+        })
+        .finally(() => context.commit('isRestocking', false));
+    },
+    dispense(context) {
+      context.commit('dispense');
+    },
+  },
+  getters: {},
+  mutations: {
+    isRestocking(state, payload) {
+      state.isRestocking = payload;
+    },
+    dispense(state) {
+      state.supply--;
+    },
+    stockItems(state) {
+      state.supply = 40;
+    },
+  },
 });
