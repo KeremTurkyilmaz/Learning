acfed8e76545320ad7aac92042cb48282b97e2eb
Kerem Türkyilmaz <kerem.turkyilmaz@outlook.it>
2022-01-22 18:27:31 +0100

    step 4 - chain actions

--- courses/frontend-masters/frontend-masters-vuex/vuex-compose-actions/src/store/index.js
+++ courses/frontend-masters/frontend-masters-vuex/vuex-compose-actions/src/store/index.js
@@ -1,64 +1,65 @@
-import Vuex from "vuex";
-import Vue from "vue";
+import Vuex from 'vuex';
+import Vue from 'vue';
 
 Vue.use(Vuex);
 
 // fake API call //
-// let inventory = {
-//   chips: {
-//     stock: 40
-//   }
-// };
+let inventory = {
+  chips: {
+    stock: 40,
+  },
+};
 
-// var pingInventory = function(item) {
-//   return new Promise(resolve => {
-//     setTimeout(function() {
-//       resolve(inventory[item]);
-//     }, 3000);
-//   });
-// };
+var pingInventory = function(item) {
+  return new Promise((resolve) => {
+    setTimeout(function() {
+      resolve(inventory[item]);
+    }, 3000);
+  });
+};
 
 let machines = {
   bender: {
-    last_serviced: "feb 2019",
-    condition: "working"
-  }
+    last_serviced: 'feb 2019',
+    condition: 'working',
+  },
 };
 
 export default new Vuex.Store({
   state: {
-    machineName: "bender",
+    machineName: 'bender',
     supply: 10,
     isRestocking: false,
     isDispensing: false,
-    isCheckingMachine: false
+    isCheckingMachine: false,
   },
   actions: {
-    fetchFromInventory() {
-      // we're going to chain actions here //
-      // we first want to dispatch an action to check if the machine is working
-      // if it is working then run an api call
-      // so something like:
-      // dispatch("checkMachineState").then(machine => { ... })
-      // remember to change the isRestocking state while you're at it
+    fetchFromInventory({ commit, dispatch }) {
+      dispatch('checkMachineState').then(() => {
+        commit('isRestocking', true);
+        pingInventory('chips').then(() => {
+          commit('isRestocking', false);
+          commit('stockItems');
+        });
+      });
     },
     dispense({ commit }) {
-      commit("isDispensing", true);
+      commit('isDispensing', true);
       setTimeout(() => {
-        commit("dispense");
-        commit("isDispensing", false);
+        commit('dispense');
+        commit('isDispensing', false);
       }, 3000);
     },
     // create an action to check machine state //
     checkMachineState({ state, commit }) {
-      commit("isCheckingMachine", true);
-      return new Promise(resolve => {
+      commit('isCheckingMachine', true);
+      return new Promise((resolve) => {
         setTimeout(() => {
-          commit("isCheckingMachine", false);
+          commit('isCheckingMachine', false);
           resolve(machines[state.machineName]);
         }, 3000);
       });
-    }
+    },
   },
   getters: {},
   mutations: {
@@ -76,6 +77,6 @@ export default new Vuex.Store({
     },
     stockItems(state) {
       state.supply = 40;
-    }
-  }
+    },
+  },
 });
