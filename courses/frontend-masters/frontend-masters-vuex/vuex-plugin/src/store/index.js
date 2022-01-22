f95e37bc3e9ac89cd62be4b87401bad90af8a8ab
Kerem Türkyilmaz <kerem.turkyilmaz@outlook.it>
2022-01-22 21:10:41 +0100

    step 7 - vuex plugin

--- courses/frontend-masters/frontend-masters-vuex/vuex-plugin/src/store/index.js
+++ courses/frontend-masters/frontend-masters-vuex/vuex-plugin/src/store/index.js
@@ -1,35 +1,35 @@
-import Vuex from "vuex";
-import Vue from "vue";
-import persistDataPlugin from "../plugins/persist-data";
+import Vuex from 'vuex';
+import Vue from 'vue';
+import persistDataPlugin from '../plugins/persist-data';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   plugins: [persistDataPlugin],
   state: {
-    machineName: "Wash Bucket",
+    machineName: 'Wash Bucket',
     lastServiced: new Date()
   },
   getters: {
     serviceDate(state) {
-      return state.lastServiced.toLocaleString("default", {
-        year: "numeric",
-        month: "short",
-        day: "numeric"
+      return state.lastServiced.toLocaleString('default', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
       });
     },
     serviceTime(state) {
-      return state.lastServiced.toLocaleString("default", {
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        timeZoneName: "short"
+      return state.lastServiced.toLocaleString('default', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZoneName: 'short'
       });
     }
   },
   actions: {
     serviceMachine({ commit }) {
-      commit("updateServiceDate", new Date());
+      commit('updateServiceDate', new Date());
     }
   },
   mutations: {
